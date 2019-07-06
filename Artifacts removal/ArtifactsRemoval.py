a1cfc359a3537d24bf2e57bfc633fc778484bcc1
Ghost29498 <32708527+Ghost29498@users.noreply.github.com>
2019-07-06 18:48:20 +0530

    Update ArtifactsRemoval.py

--- Artifacts removal/ArtifactsRemoval.py
+++ Artifacts removal/ArtifactsRemoval.py
@@ -10,51 +10,52 @@ import numpy as np
 from PIL import Image
 from ISR.models import RDN
 
-"""
-  Initializes the model
-  Parameters:
-    weightPath: 'path to weights'
-  Returns:
-    rdn: 'model'
-    
-"""
-def init(weightPath):
-  rdn = RDN(arch_params={'C':6, 'D':20, 'G':64, 'G0':64, 'x':2})
-  rdn.model.load_weights(weightPath)
-  return(rdn)
+class ArtifactsRemoval:
+  def __init__(self,weightPath):
+    """
+    Initializes the model
+    Parameters:
+      weightPath: 'path to weights'
+    Architecture Parameters:
+       - D  - number of Residual Dense Blocks (RDB)
+       - C  - number of convolutional layers stacked inside a RDB
+       - G  - number of feature maps of each convolutional layers inside the RDBs 
+       - G0 - number of feature maps for convolutions outside of RDBs and of each RBD output
+       - x  - upscale parameter
+    """
+    rdn = RDN(arch_params={'C':6, 'D':20, 'G':64, 'G0':64, 'x':2})
+    rdn.model.load_weights(weightPath)
+    self.rdn = rdn
   
-"""
-  passes the image to the model to remove artifacts
-  Parameters:
-    img: 'image content'
-    rdn: 'model'
-  Returns:
-    img_hr: 'The image content without Artifacts'
-"""
-def process(img,rdn):
-  lr_img = np.array(img)
-  sr_img = rdn.predict(lr_img)
-  img_hr = Image.fromarray(sr_img)
-  return(img_hr)
-  
-  """
-  The image dimention is reduced into half of its orginal size and then process it
-  Parameters: 
-    imgPath: 'Path to image'
-    rdn: 'model'
-  Returns:
-    img_hr: The image content without Artifacts
-"""
-def removeArtifacts(imgPath,rdn):
-  img = Image.open(imgPath)
-  size = img.width//2,img.height//2 
-  img = img.resize(size)
-  img_hr = process(img,rdn)
-  return(img_hr)
+  def process(self,img):
+    """
+    passes the image to the model to remove artifacts
+    Parameters:
+      img: 'image content'
+    Returns:
+      img_hr: 'The image content without Artifacts'
+    """
+    lr_img = np.array(img)
+    sr_img = self.rdn.predict(lr_img)
+    img_hr = Image.fromarray(sr_img)
+    return(img_hr)
+  def removeArtifacts(self,imgPath):
+    """
+    The image dimention is reduced into half of its orginal size and then process it
+    Parameters: 
+      imgPath: 'Path to image'
+    Returns:
+      img_hr: The image content without Artifacts
+    """
+    img = Image.open(imgPath)
+    size = img.width//2,img.height//2 
+    img = img.resize(size)
+    img_hr = process(img)
+    return(img_hr)
   
 #sample code for using the module
 if __name__ == "__main__":
     weightPath = 'rdn-C6-D20-G64-G064-x2_ArtefactCancelling_epoch219.hdf5'
-    rdn = init(weightPath)
     imgPath = "E:/IND0006216.jpg"
-    img_hr = removeArtifacts(imgPath,rdn)
+    artifactsRemoval = ArtifactsRemoval(weightPath)
+    img_hr = artifactsRemoval.removeArtifacts(imgPath)
